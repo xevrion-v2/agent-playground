14c1060c6fb196d7d91860446c7b45757ccad5ad
Iván Rodríguez <ivandejesus1806@gmail.com>
2026-06-03 19:10:00 -0600

    fix ui button jsx output

---
+++ packages/ui/test/button.test.tsx
@@ -0,0 +1,7 @@
+import { Button } from "../src";
+
+const enabledButton = <Button label="Save task" aria-label="Save task" />;
+const disabledButton = <Button label="Delete task" disabled />;
+
+void enabledButton;
+void disabledButton;
