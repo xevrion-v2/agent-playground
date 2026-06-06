ecf0479df4f781df66ba553d10a5a1859c960d47
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-06 13:51:26 -0400

    fix: apply solution for issue #12

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -1,10 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest',
   },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
 };
