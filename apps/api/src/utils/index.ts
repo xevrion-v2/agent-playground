b6281c5a8fcec6fa72dd98ceba5fc3f9ccc57ae3
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-06 06:25:43 -0400

    fix: apply solution for issue #7

---
+++ apps/api/src/utils/index.ts
@@ -0,0 +1,5 @@
+export * from './apiError';
+
+// If this file already exists, this would be added to the existing exports
+// If it doesn't exist, a new file would be created with this content
+export { APIError } from './apiError';
