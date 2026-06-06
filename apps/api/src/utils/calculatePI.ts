44b6af5ab247906b4da4cfbad91e2a80a7c98b9d
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-06 12:32:26 -0400

    fix: apply solution for issue #14

--- apps/api/src/utils/calculatePI.ts
+++ apps/api/src/utils/calculatePI.ts
@@ -1,22 +1,23 @@
 /**
- * Calculates the value of PI using the Leibniz formula
- * @returns {number} The calculated value of PI
+ * Calculates the value of PI using the Leibniz formula for π
+ * 
+ * The Leibniz formula is an infinite series that converges to π:
+ * π = 4/1 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11 + 4/13 - ...
+ * 
+ * @param iterations - Number of iterations to use in the series (higher = more accurate)
+ * @returns The calculated value of PI
  */
-export function calculatePI(): number {
+export function calculatePI(iterations: number = 10000000): number {
   let pi = 0;
-  const iterations = 1000000;
+  let denominator = 1;
+  
   for (let i = 0; i < iterations; i++) {
-    pi += Math.pow(-1, i) / (2 * i + 1);
+    if (i % 2 === 0) {
+      pi += 4 / denominator;
+    } else {
+      pi -= 4 / denominator;
+    }
+    denominator += 2;
   }
-  return pi * 4;
+  return pi;
 }
-
-/**
- * Alternative PI calculation using Monte Carlo method
- * @returns {number} The calculated value of PI
- */
-export function calculatePIMonteCarlo(): number {
-  const iterations = 1000000;
-  let insideCircle = 0;
-  for (let i = 0; i < iterations; i++) {
-    const x = Math.random();
