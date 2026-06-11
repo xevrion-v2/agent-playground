1bdb49df1f54b06482bb15cd69adf4c9b9de1503
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-10 20:23:10 -0400

    fix: apply solution for issue #8

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -4,3 +4,11 @@ import routes from './routes';
 import { getHealth } from './controllers/health.controller';
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+// Health check
+app.get('/health', getHealth);
+
+// API routes
+app.use('/api', routes);
