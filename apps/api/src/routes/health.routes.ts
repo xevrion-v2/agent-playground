dd5e8bfd19ed5e8613d74fbcb2225f7282c2e73c
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-29 12:37:51 -0400

    fix: apply solution for issue #8

--- apps/api/src/routes/health.routes.ts
+++ apps/api/src/routes/health.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { successResponse } from '../utils/response';
 
 const router = Router();
 
 router.get('/', (_req, res) => {
-  res.json(successResponse({
-    healthy: true
-  }));
+  res.json({
+    status: 'success',
+    data: { healthy: true }
+  });
 });
 
 export default router;
