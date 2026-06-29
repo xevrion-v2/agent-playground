af6d06cd3b1d98c4373f659d27402358098c86d1
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-29 12:37:55 -0400

    fix: apply solution for issue #8

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,1 +1,11 @@
-import './utils/response';
+import express from 'express';
+import routes from './routes';
+import healthRoutes from './routes/health.routes';
+
+const app = express();
+
+
+// Health check endpoint
+app.use('/health', healthRoutes);
+
+app.use('/api', routes);
