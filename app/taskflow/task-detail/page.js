2d3d25904b9771e817345204a9b885717eea87bc
mkcash <mkcashcom@gmail.com>
2026-06-30 10:45:55 +0800

    [baobao] Implement [Bounty] Add TaskFlow task detail route placeholder
    Fixes #0

---
+++ app/taskflow/task-detail/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TaskDetailPage() {
+  return (
+    <div>
+      <h1>TaskFlow Task Detail</h1>
+      <p>This is a placeholder page for TaskFlow task detail.</p>
+    </div>
+  );
+}
