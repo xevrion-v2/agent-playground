a80a04380afe9c0bad876be2cdb42b31ac4cb891
ChaiXinLong-tech <155515635+ChaiXinLong-tech@users.noreply.github.com>
2026-07-01 12:08:51 +0800

    feat: add has-own-key utility

---
+++ apps/api/src/utils/has-own-key.ts
@@ -0,0 +1,6 @@
+export function hasOwnKey<T extends object, K extends PropertyKey>(
+  value: T,
+  key: K,
+): value is T & Record<K, unknown> {
+  return Object.prototype.hasOwnProperty.call(value, key);
+}
