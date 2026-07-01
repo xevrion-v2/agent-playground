8b10067d850e85f6eb72ad40814a9e8bd1b3abd6
Evelyn <evelynxuu1103@gmail.com>
2026-07-02 01:55:45 +0800

    feat(api): add toSlugCase

---
+++ apps/api/src/utils/to-slug-case.ts
@@ -0,0 +1,8 @@
+export function toSlugCase(value: string): string {
+  return value
+    .replace(/([a-z0-9])([A-Z])/g, "$1-$2")
+    .replace(/[\s_]+/g, "-")
+    .replace(/-+/g, "-")
+    .replace(/^-+|-+$/g, "")
+    .toLowerCase();
+}
