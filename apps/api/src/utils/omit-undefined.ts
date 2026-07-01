9933315d9b6d31b5ed636342336c898b17da9ef8
ChaiXinLong-tech <155515635+ChaiXinLong-tech@users.noreply.github.com>
2026-07-01 11:06:19 +0800

    feat(api): add omit-undefined helper

---
+++ apps/api/src/utils/omit-undefined.ts
@@ -0,0 +1,5 @@
+export function omitUndefined<T extends Record<string, unknown>>(value: T): Partial<T> {
+  const entries = Object.entries(value).filter(([, entryValue]) => entryValue !== undefined);
+
+  return Object.fromEntries(entries) as Partial<T>;
+}
