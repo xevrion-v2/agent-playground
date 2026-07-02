ac864c2c95f6ba8766b03c8a2ecb4b59bce893a5
wisdom518 <2393347493@qq.com>
2026-07-03 03:20:03 +0800

    feat(api): add request logger middleware

---
+++ apps/api/src/middleware/request-logger.ts
@@ -0,0 +1,17 @@
+import type { RequestHandler } from "express";
+
+export const requestLogger: RequestHandler = (req, res, next) => {
+  const startedAt = Date.now();
+
+  res.on("finish", () => {
+    const durationMs = Date.now() - startedAt;
+
+    console.info(
+      `${req.method} ${req.originalUrl} ${res.statusCode} ${durationMs}ms`,
+    );
+  });
+
+  next();
+};
+
+export default requestLogger;
