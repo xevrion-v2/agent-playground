a38c77deb6feaac02dda65fb6b983445036211aa
Lokas73737 <jordanbray254@gmail.com>
2026-08-21 01:24:48 +0300

    Update users.ts

--- apps/api/src/routes/users.ts
+++ apps/api/src/routes/users.ts
@@ -2,6 +2,14 @@ import { Router } from "express";
 
 const router = Router();
 
+/**
+ * List all users.
+ *
+ * @route GET /users
+ * @returns {object} 200 - An object containing the list of users
+ * @returns {Array}  .data - Array of user objects
+ * @returns {string} .message - Status message for the request
+ */
 router.get("/", (_req, res) => {
   res.json({
     data: [],
@@ -9,6 +17,15 @@ router.get("/", (_req, res) => {
   });
 });
 
+/**
+ * Create a new user.
+ *
+ * @route POST /users
+ * @param {object} req.body - The user payload to create
+ * @returns {object} 201 - An object containing the created user
+ * @returns {object} .data - The created user object (stubbed)
+ * @returns {string} .message - Status message for the request
+ */
 router.post("/", (req, res) => {
   res.status(201).json({
     data: {
